098224c7ad937fa4e942e9e1a030a2351564802d
Jake Bisson <jlb4@rogers.com>
2024-05-17 18:59:58 -0400

    setup https server

--- server/index.js
+++ server/index.js
@@ -1,26 +1,31 @@
-require('dotenv-safe').config();
+require('dotenv-safe').config({
+  allowEmptyValues: true,
+});
 const express = require('express');
 const helmet = require('helmet');
 const http = require('http');
+const https = require('https');
 const path = require('path');
 const fs = require('fs');
-const disableTraceMethod = require('./middleware/disableTraceMethod');
 
+// Define the express app
 const app = express();
 
+// Configure Helmet
 app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
-        'script-src-attr': ["'self'", "'unsafe-inline'"],
+        'script-src-attr': ["'unsafe-inline'"],
       },
     },
   }),
 );
-app.use(disableTraceMethod);
 
+// Root directory
 const root = path.join(__dirname, '../dist/angular-17-node-deploy/browser');
 
+// Ensure all routes direct the user to the application
 app.get('*', function (req, res) {
   fs.stat(root + req.path, function (err) {
     if (err) {
@@ -31,8 +36,21 @@ app.get('*', function (req, res) {
   });
 });
 
-const port = process.env.PORT || 8080;
-const server = http.createServer(app);
-server.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
-});
+// Start the server
+const { SSL_CERT_NAME, SSL_KEY_NAME } = process.env;
+const port = SSL_CERT_NAME && SSL_KEY_NAME ? 443 : 80;
+if (SSL_CERT_NAME && SSL_KEY_NAME) {
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', SSL_KEY_NAME)),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', SSL_CERT_NAME)),
+  };
+  const server = https.createServer(options, app);
+  server.listen(port, () => {
+    console.log(`https server is running on port ${port}`);
+  });
+} else {
+  const server = http.createServer(app);
+  server.listen(port, () => {
+    console.log(`http server is running on port ${port}`);
+  });
+}
--- server/middleware/disableTraceMethod.js
+++
@@ -1,8 +0,0 @@
-const disableTraceMethod = (req, res, next) => {
-  if (req.method === 'TRACE') {
-    return res.sendStatus(405);
-  }
-  next();
-};
-
-module.exports = disableTraceMethod;
